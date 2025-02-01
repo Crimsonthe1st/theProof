a782da7dd86551a9e47920d5af4d69aa5c1327f8
kenny <kennycs@protonmail.com>
2025-02-01 10:24:49 -0500

    Slight header changes

--- hack-davidson/app/Pages/Header.js
+++ hack-davidson/app/Pages/Header.js
@@ -1,16 +1,11 @@
 import React from 'react';
 
-
 function Header(){
    return(
    <header className="header">
-   {
        <div className='logo'> THE PROOF LOGO</div>
-       <div className ='profile' style={{display: flex; justify-content: center;}}>PROFILE </div>
-   }
+       <div className ='profile' style={{display: flex; justify-content: center;}}>PROFILE</div>
    </header>
    )
-
-
 }
 export default Header;
