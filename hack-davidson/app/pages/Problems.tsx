01e8811c0a3ca8ddf06ad817f23341b5a101c6b4
kenny <kennycs@protonmail.com>
2025-02-01 11:38:52 -0500

    Added working changes

---
+++ hack-davidson/app/pages/Problems.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function Problems() {
+   return (
+      Problem()
+   )
+}
+
+function Problem() {
+   return (
+      <div className="flex flex-col gap-2 p-8 sm:flex-row sm:items-center sm:gap-6 sm:py-4">
+         <div className="space-y-2 text-center sm:text-left">
+            <div className="space-y-0.5">
+               <p className="text-lg font-semibold text-black"></p>
+            </div>
+         </div>
+      </div>
+   )
+}
+
+export default Problems;
