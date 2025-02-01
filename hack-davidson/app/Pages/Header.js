78c1c3e06240e6a102ab348fd2a56d03b3873236
Juliet B <jbellionthe1st@gmail.com>
2025-02-01 10:15:52 -0500

    added folder for pages

---
+++ hack-davidson/app/Pages/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+function Header(){
+   return(
+   <header className="header">
+   {
+       <div className='logo'> THE PROOF LOGO</div>
+       <div className ='profile' style={{display: flex; justify-content: center;}}>PROFILE </div>
+   }
+   </header>
+   )
+
+
+}
+export default Header;
